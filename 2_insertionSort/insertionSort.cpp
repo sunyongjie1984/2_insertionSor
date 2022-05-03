f8b6779a52c0bace3187b7d7e31246059d885742
yongjies <yongjies@cadence.com>
2022-05-03 23:09:22 +0800

    add

---
+++ 2_insertionSort/insertionSort.cpp
@@ -0,0 +1,30 @@
+// InsertionSort.cpp
+#include "insertionSort.h"
+
+#ifdef __linux
+#include <stdio.h>
+#endif
+
+// windows7 and visual studio is for viewing code
+// just for viewing
+// and you can edit files and checkin in the mac os in macbook pro
+
+int main()
+{
+	uint64_t array[6] = { 5, 2, 4, 6, 1, 3 };
+
+	for (uint64_t i = 0; i < sizeof(array) / sizeof(uint64_t); ++i)
+	{
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+	InsertionSort(array, sizeof(array) / sizeof(uint64_t));
+	for (uint64_t i = 0; i < sizeof(array) / sizeof(uint64_t); ++i)
+	{
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "press any key to end this program" << std::endl;
+	getchar();
+	return 0;
+}
---
+++ 2_insertionSort/insertionSort.h
@@ -0,0 +1,37 @@
+#pragma once
+// InsertionSort.h
+#include <iostream>
+#include <stdint.h>
+// INSERTION-SORT(A)
+// for j = 2 to A.length
+//     key = A[j]
+//     // Insert A[j] into sorted sequence A[1..j - 1].
+//     i = j - 1ls
+//     while i > 0 and a[i] > key
+//         a[i + 1] = a[i]
+//         i = i - 1
+//     a[i + 1] = key
+
+// allow the same key
+
+void InsertionSort(uint64_t* const A, uint64_t const n)
+{
+	uint64_t key;
+	int64_t i;
+	// j = 2 to A.length(j 1 to n - 1)
+	for (uint64_t j = 1; j < n; ++j) // j begin with A[1]
+	{
+		key = A[j]; // we are putting A[j] (key) to the right place
+		i = j - 1;    // for every A[j], compare begin the previous one
+		// very element ahead of A[j] is sorted
+		// move back one step for every element who is bigger than A[j]
+		while (i >= 0 && A[i] > key)
+		{
+			A[i + 1] = A[i];
+			--i;
+		}
+		// after moving, A[i] is equal or less then A[j]
+		// A[j] shoule be after A[i]
+		A[i + 1] = key;
+	}
+}
